6179492c310d36c3605f5dcbc9bf7b3bd026f291
ousidus <olgierdusidus@gmail.com>
2020-08-02 15:46:16 +0200

    working shipping and transcton for registered user

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import *
+
 # Create your views here.
 def store(request):
     
@@ -71,3 +73,29 @@ def update_item(request):
     
     return JsonResponse("Item was added", safe = False)
 
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete= False)
+    else:
+        customer, order = guestOrder(request, data)
+        
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+    
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+    
+    if order.shipping == True:
+        ShippingAddress.objects.create(customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],)
+    
+    return JsonResponse('Payment subbmitted.if.',safe=False) 
